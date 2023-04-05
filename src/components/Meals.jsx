352d993abb38b6d2c0414d99a64c0c252d5fb5f2
szvitek <gy.szvitek@gmail.com>
2023-04-05 23:21:13 +0100

    feat(meals): meals component implemented

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,39 @@
 import { useAppContext } from '../context';
+import { BsHandThumbsUp } from 'react-icons/bs';
 
 const Meals = () => {
-  const context = useAppContext();
-  console.log(context);
-  return <div>Meals</div>;
+  const { loading, meals } = useAppContext();
+  if (loading) {
+    return (
+      <section className="section">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (!meals.length) {
+    return (
+      <section className="section">
+        <h4>No meals matched your search term. Please try again.</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="section-center">
+      {meals.map(({ idMeal: id, strMeal: title, strMealThumb: image }) => (
+        <article key={id} className="single-meal">
+          <img src={image} alt={title} className="img" />
+          <footer>
+            <h5>{title}</h5>
+            <button className="like-btn">
+              <BsHandThumbsUp />
+            </button>
+          </footer>
+        </article>
+      ))}
+    </section>
+  );
 };
 
 export default Meals;
--- src/context.jsx
+++ src/context.jsx
@@ -1,24 +1,54 @@
-import { createContext, useContext, useEffect } from 'react';
+import axios from 'axios';
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 const AppContext = createContext();
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a';
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
 const AppProvider = ({ children }) => {
-  const fetchData = async () => {
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  /*
+  Why useCallback? Well, since the function is declared outside of useEffect, you
+  will have to put it in the dependency array of the hook. But if the
+  function isn't wrapped in useCallback, it will update on every
+  re-render, and thus trigger the useEffect on every re-render. Which is
+  rarely what you want!
+  */
+  const fetchMeals = useCallback(async (url) => {
     try {
-      const data = await fetch('https://randomuser.me/api');
-      const json = await data.json();
-      console.log(json);
+      setLoading(true);
+      const { data } = await axios.get(url);
+      if (data.meals) {
+        setMeals(data.meals);
+      } else {
+        setMeals([]);
+      }
     } catch (error) {
-      console.log(error);
+      console.log(error.response);
+    } finally {
+      setLoading(false);
     }
-  };
+  }, []);
 
   useEffect(() => {
     // call the function
-    fetchData();
-  }, [fetchData]);
+    fetchMeals(allMealsUrl);
+  }, [fetchMeals]);
 
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ loading, meals }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // custom hook make consuming AppContext easier
