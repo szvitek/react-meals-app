3482239268ca02b46c8c62ea43466585285d4dee
szvitek <gy.szvitek@gmail.com>
2023-04-05 20:03:50 +0100

    feat(contextApi): data fetching and useEffect hook

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,23 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect } from 'react';
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  const fetchData = async () => {
+    try {
+      const data = await fetch('https://randomuser.me/api');
+      const json = await data.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // call the function
+    fetchData();
+  }, [fetchData]);
+
   return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
